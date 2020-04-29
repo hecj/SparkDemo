28587cdf670262baa41ee35a6cbda2fef8c61a54
何超杰 <hechaojie@xylink.cn>
2020-04-29 11:44:07 +0800

    两数之和

--- src/main/scala/arithmetic/leetcode/Problem20200428v2.java
+++ src/main/scala/arithmetic/leetcode/Problem20200428v2.java
@@ -38,32 +38,40 @@ public class Problem20200428v2 {
 	
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 		// l1的真实数据
-		int total1 = 0;
-		int step = 1;
-		while(l1!=null){
-			total1 = total1+l1.val*step;
-			step *= 10;
-			l1 = l1.next;
-		}
-		int total2 = 0;
-		int step2 = 10;
-		while(l2!=null){
-			total2 = total2+l2.val*step2;
-			step2 *= 10;
-			l2 = l2.next;
-		}
-		int total3= total1+total2;
-		System.out.println(total3);
 		ListNode list = new ListNode(0);
 		ListNode temp = list;
-		if(total3<10){
-			return new ListNode(total3);
-		}
-		while(total3>0){
-			int val = total3%10;
-			total3 = total3/10;
-			temp.next = new ListNode(val);
-			temp = temp.next;
+		int addNum = 0;
+		while(l1 != null || l2 != null || addNum !=0){
+			
+			if(l1 == null && l2 == null){
+				if(addNum>0){
+					temp.next = new ListNode(addNum);
+					break;
+				}
+			}
+			
+			int l1Num = 0,l2Num = 0;
+			// 两数之和
+			if(l1 != null){
+				l1Num = l1.val;
+				l1 = l1.next;
+			}
+			if(l2 != null){
+				l2Num = l2.val;
+				l2 = l2.next;
+			}
+			int num = l1Num + l2Num+addNum;
+			addNum = 0;
+			if(num < 10){
+				temp.next = new ListNode(num);
+				temp = temp.next;
+			} else{
+				// 大于10,进1
+				int val = num%10;
+				addNum = num/10;
+				temp.next = new ListNode(val);
+				temp = temp.next;
+			}
 		}
 		return list.next;
 	}
