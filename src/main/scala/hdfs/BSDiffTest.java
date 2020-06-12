0bf6da0167b07a231ce18111e88e8528d02373be
何超杰 <hechaojie@xylink.cn>
2020-06-12 16:49:17 +0800

    1

--- src/main/scala/hdfs/BSDiffTest.java
+++ src/main/scala/hdfs/BSDiffTest.java
@@ -11,8 +11,7 @@ public class BSDiffTest {
         File oldFile = new File("/Users/hecj/Desktop/file3/1.docx");
         File newFile = new File("/Users/hecj/Desktop/file3/2.docx");
         File diffFile = new File("/Users/hecj/Desktop/file3/1.patch");
-//        BSDiff.bsdiff(oldFile,newFile,diffFile);
-
+        BSDiff.bsdiff(oldFile,newFile,diffFile);
         BSPatch.patchFast(oldFile,new File("/Users/hecj/Desktop/file3/1_new.docx"),diffFile,0);
     }
 }
